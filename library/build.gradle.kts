a0892ea068618618e9cf8de8007c326fa9072965
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-04 09:37:57 +0530

    Enable Kotlin Multiplatform targets for JVM, iOS, and experimental WASM in the library module

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,5 +1,21 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+}
+
+kotlin {
+    jvm()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        binaries.executable()
+    }
 }
 
 // Custom build directory
