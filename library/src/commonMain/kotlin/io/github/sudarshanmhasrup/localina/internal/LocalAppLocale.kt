19c691991c4b0b45897c3cde2f3a754f7565922b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-04 21:16:44 +0530

    Remove `LocalAppLocale` internal object declaration

--- library/src/commonMain/kotlin/io/github/sudarshanmhasrup/localina/internal/LocalAppLocale.kt
+++
@@ -1,9 +0,0 @@
-package io.github.sudarshanmhasrup.localina.internal
-
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ProvidedValue
-
-internal expect object LocalAppLocale {
-    @Composable
-    infix fun provides(value: String?): ProvidedValue<*>
-}
