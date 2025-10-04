70319f8abf154a6ba26f27914c103334a65036de
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-04 20:09:00 +0530

    Add ComposeAppTheme to define light and dark color schemes and integrate custom typography

---
+++ compose-app/src/commonMain/kotlin/com/compose/app/presentation/theme/Theme.kt
@@ -0,0 +1,34 @@
+package com.compose.app.presentation.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val lightColorScheme = lightColorScheme(
+    background = backgroundColorLight,
+    primary = primaryColorCommon,
+    onSurface = primaryFontColorLight,
+    onSurfaceVariant = secondaryFontColorLight,
+)
+
+private val darkColorScheme = darkColorScheme(
+    background = backgroundColorDark,
+    onSurface = primaryFontColorDark,
+    onSurfaceVariant = secondaryFontColorDark,
+)
+
+@Composable
+fun ComposeAppTheme(
+    isDarkMode: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (isDarkMode) darkColorScheme else lightColorScheme
+    val typography = getTypography()
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = typography,
+        content = content
+    )
+}
