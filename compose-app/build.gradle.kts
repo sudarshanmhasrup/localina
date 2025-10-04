f35f5296b427c51917bfd49675e04eca50aba8c9
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-04 17:06:28 +0530

    Define the compose-app module's configuration information from the version catalog

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
 }
 
+group = libs.versions.composeApp.group.get()
+version = libs.versions.composeApp.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/ComposeApp"))
