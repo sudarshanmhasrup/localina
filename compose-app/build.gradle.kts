11df71be242f7f95f1d8d5e8837c508ee470d413
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-05 19:03:39 +0530

    Apply the "com.android.library" plugin to the compose-app module

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
 }
