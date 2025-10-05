87ea3d82daf6370a2f31c84d410e08abcd2da916
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-05 19:23:15 +0530

    Add AndroidApp composable entry point for Android target

---
+++ compose-app/src/androidMain/kotlin/com/compose/app/AndroidApp.kt
@@ -0,0 +1,9 @@
+package com.compose.app
+
+import androidx.compose.runtime.Composable
+import com.compose.app.presentation.composeApp.ComposeApp
+
+@Composable
+fun AndroidApp() {
+    ComposeApp()
+}
