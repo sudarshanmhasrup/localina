4138f6abbc9bcd34ac82937897c99256721fec5e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-06 18:41:56 +0530

    Apply the "com.android.library" plugin to the library module

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -4,6 +4,7 @@ import com.vanniktech.maven.publish.SonatypeHost
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.maven.publish)
 }
