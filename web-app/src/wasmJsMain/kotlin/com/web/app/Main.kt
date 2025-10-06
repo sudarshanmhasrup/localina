919a62383321c8bc2251730c843ed18adac16b4b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-06 18:01:55 +0530

    Introduce `Main.kt` entry point for wasmJsMain module with Koin initialization and WebApp setup

---
+++ web-app/src/wasmJsMain/kotlin/com/web/app/Main.kt
@@ -0,0 +1,19 @@
+package com.web.app
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+import com.compose.app.WebApp
+import com.compose.app.di.initKoin
+import kotlinx.browser.document
+import org.jetbrains.skiko.wasm.onWasmReady
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    initKoin()
+    onWasmReady {
+        val body = document.body ?: return@onWasmReady
+        ComposeViewport(viewportContainer = body) {
+            WebApp()
+        }
+    }
+}
