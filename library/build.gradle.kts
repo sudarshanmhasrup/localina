ae78a87839b092aebe6a1902867c31d9ece2bc3d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-08 23:53:34 +0530

    Update build.gradle.kts

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -3,8 +3,8 @@ import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.maven.publish)
 }
