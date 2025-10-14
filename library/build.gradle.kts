ea7a02bc98401c42a02755737378e5ea49987942
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-14 21:13:40 +0530

    Apply the "com.android.library" plugin to the library module

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.application)
 }
 
 group = libs.versions.library.group.get()
