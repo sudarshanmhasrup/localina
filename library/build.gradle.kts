c62c8f41166024beecc5cb36cdbb5e4e88e32718
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-14 21:07:37 +0530

    Define the library module's configuration information from the version catalog

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
 }
 
+group = libs.versions.library.group.get()
+version = libs.versions.library.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/Library"))
