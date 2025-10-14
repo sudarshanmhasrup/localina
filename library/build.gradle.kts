7b077860fa50ab512546aaad81cdb26de2a8f9d9
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-14 21:09:37 +0530

    Setup Kotlin Multiplatform targets for the library module

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,9 +1,27 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
 }
 
 group = libs.versions.library.group.get()
 version = libs.versions.library.version.get()
+
+kotlin {
+    androidTarget()
+
+    jvm()
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        binaries.executable()
+    }
+}
 
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/Library"))
