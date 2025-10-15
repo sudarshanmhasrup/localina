5cff7e1a289f9065040a26680467bb184e0cf0fc
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-15 08:19:01 +0530

    Add the "org.jetbrains.compose" plugin to the root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
 }
 
 // Custom build directory
