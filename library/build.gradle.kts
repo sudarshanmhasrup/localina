8283c5adaa347fc9f9271844fa4e3f971e58e8aa
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-15 08:24:07 +0530

    Apply the "org.jetbrains.kotlin.plugin.compose" plugin to the library module

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
     alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
 }
 
 group = libs.versions.library.group.get()
