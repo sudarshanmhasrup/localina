e7dc6c8d01dd2733937f0a229dea5fc28bc326bd
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-15 14:46:26 +0530

    Add the Kotlin Multiplatform setup the library module
    - Add wasmJs target.
    - Apply the Kotlin Multi-platform plugin.
    - Apply the Compose Multiplatform plugin.
    - Apply the Compose Compiler plugin.

--- wasm-app/build.gradle.kts
+++ wasm-app/build.gradle.kts
@@ -1,5 +1,25 @@
-plugins {
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalDistributionDsl
 
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+}
+
+kotlin {
+    @OptIn(
+        ExperimentalWasmDsl::class,
+        ExperimentalDistributionDsl::class
+    )
+    wasmJs {
+        browser {
+            distribution {
+                outputDirectory = File("$rootDir/.build/WebAppDistribution")
+            }
+        }
+        binaries.executable()
+    }
 }
 
 // Custom build directory
