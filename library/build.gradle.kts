188b15f2a4f02a9abd98b388bae0c312fe4efc0f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-16 20:39:41 +0530

    Update the reference of android-library plugin to android-kotlin-multiplatform-library in the library module's build.gradle.kts

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.maven.publish)
