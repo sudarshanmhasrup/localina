7e367559d6a36cad9bb5d4c8ed255bad59592e4e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-16 20:47:02 +0530

    Configure multiplatform setup for compose-app module
    - Add Android, JVM, iOS and Wasm targets.
    - Setup Android library configurations.
    - Define the compose-app module's configuration information

--- compose-app/build.gradle.kts
+++ compose-app/build.gradle.kts
@@ -1,5 +1,57 @@
-plugins {
+import com.android.build.api.dsl.androidLibrary
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
+}
+
+group = libs.versions.composeApp.group.get()
+version = libs.versions.composeApp.version.get()
+
+kotlin {
+    @Suppress("UnstableApiUsage")
+    androidLibrary {
+        namespace = libs.versions.library.androidLibrary.namespace.get()
+        compileSdk = libs.versions.library.androidLibrary.compileSdk.get().toInt()
+        minSdk = libs.versions.library.androidLibrary.minSdk.get().toInt()
+
+        compilations.configureEach {
+            compilerOptions.configure {
+                jvmTarget.set(
+                    JvmTarget.JVM_11
+                )
+            }
+        }
+    }
+
+    jvm()
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        binaries.executable()
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.compose.multiplatform.ui)
+        }
+    }
 }
 
 // Custom build directory
