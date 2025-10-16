5996c6bf994ae2356f300d1352e9f61ece8a15b4
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-16 21:15:17 +0530

    Add typography definitions with custom font family in compose-app theme

---
+++ compose-app/src/commonMain/kotlin/com/compose/app/presentation/theme/Theme.kt
@@ -0,0 +1,35 @@
+package com.compose.app.presentation.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val lightColorScheme = lightColorScheme(
+    background = backgroundColorLight,
+    primary = primaryColorCommon,
+    onSurface = primaryFontColorLight,
+    onSurfaceVariant = secondaryFontColorLight,
+)
+
+private val darkColorScheme = darkColorScheme(
+    background = backgroundColorDark,
+    onSurface = primaryFontColorDark,
+    onSurfaceVariant = secondaryFontColorDark,
+)
+
+@Composable
+fun ComposeAppTheme(
+    isDarkMode: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (isDarkMode) darkColorScheme else lightColorScheme
+    val typography = getTypography()
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = typography,
+        content = content
+    )
+}
