acf69ae6f7b4c26277c7e93c1b84dcbef0e7fd36
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-17 19:43:42 +0530

    Add `backgroundModifier` extension for setting background color

--- compose-app/src/commonMain/kotlin/com/compose/app/presentation/extensions/Modifiers.kt
+++ compose-app/src/commonMain/kotlin/com/compose/app/presentation/extensions/Modifiers.kt
@@ -1,9 +1,20 @@
 package com.compose.app.presentation.extensions
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+
+@Composable
+internal fun Modifier.backgroundModifier(backgroundColor: Color): Modifier {
+    return this
+        .fillMaxSize()
+        .background(color = backgroundColor)
+}
 
 internal fun Modifier.headingAndMessageModifier(isMobileLandscapeMode: Boolean = false): Modifier {
     val topPadding = if (isMobileLandscapeMode) 20.dp else 60.dp
