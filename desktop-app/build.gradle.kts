01d3dc2c0dd54fd7d462d827297f20c67e594153
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-23 11:01:54 +0530

    Configure JVM toolchain to use Java 21 in `build.gradle.kts`

--- desktop-app/build.gradle.kts
+++ desktop-app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
@@ -9,7 +11,21 @@ group = libs.versions.desktopApp.group.get()
 version = libs.versions.desktopApp.version.get()
 
 kotlin {
-    jvm()
+    jvm {
+        java {
+            @Suppress("UnstableApiUsage")
+            toolchain {
+                languageVersion.set(JavaLanguageVersion.of("21"))
+                vendor.set(JvmVendorSpec.JETBRAINS)
+            }
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
+        }
+
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_21
+        }
+    }
 
     sourceSets {
         jvmMain.dependencies {
