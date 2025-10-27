b31c5600d52e0414a51390b8d2c2e8e02096038e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-10-27 09:21:21 +0530

    mark `Locale` enum with `@ExperimentalLocalinaApi` annotation

--- library/src/commonMain/kotlin/io/github/sudarshanmhasrup/localina/api/Locale.kt
+++ library/src/commonMain/kotlin/io/github/sudarshanmhasrup/localina/api/Locale.kt
@@ -1,11 +1,14 @@
 package io.github.sudarshanmhasrup.localina.api
 
+import io.github.sudarshanmhasrup.localina.annoations.ExperimentalLocalinaApi
+
 /**
  * An enum class that defines supported languages in the application with their corresponding ISO 639-1 language codes.
  * Each enum constant represents a language and is associated with its two-letter language code.
  *
  * @property code The two-letter ISO 639-1 language code for the locale
  */
+@ExperimentalLocalinaApi
 public enum class Locale(val code: String) {
     English(code = "en"),
     Marathi(code = "mr"),
