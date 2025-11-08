3998d8916a9685115d97b20e9fb059f761dc0241
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-11-08 17:34:35 +0530

    #2 fix(android): prevent crash by not closing AssetManager during configuration changes

--- library/src/androidMain/kotlin/io/github/sudarshanmhasrup/localina/internal/LocalAppLocale.android.kt
+++ library/src/androidMain/kotlin/io/github/sudarshanmhasrup/localina/internal/LocalAppLocale.android.kt
@@ -1,41 +1,36 @@
 package io.github.sudarshanmhasrup.localina.internal
 
-import android.content.Context
+import android.content.res.Configuration
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ProvidedValue
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalResources
 import java.util.Locale
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 internal actual object LocalAppLocale {
     private var defaultLocale: Locale? = null
-    private var appContext: Context? = null
 
     @Composable
     actual infix fun provides(value: String?): ProvidedValue<*> {
-        val configuration = LocalConfiguration.current
+        val context = LocalContext.current
+        val currentConfig = LocalConfiguration.current
 
         if (defaultLocale == null) {
             defaultLocale = Locale.getDefault()
         }
 
-        val new = when (value) {
+        val newLocale = when (value) {
             null -> defaultLocale!!
             else -> Locale.forLanguageTag(value)
         }
-        Locale.setDefault(new)
-        configuration.setLocale(new)
-        val resources = LocalResources.current
+        Locale.setDefault(newLocale)
 
-        if (appContext == null) {
-            appContext = LocalContext.current
+        val newConfig = Configuration(currentConfig).apply {
+            setLocale(newLocale)
         }
-        val context = appContext!!.createConfigurationContext(configuration)
-        resources.assets.close()
-        context.resources
+        context.createConfigurationContext(newConfig)
 
-        return LocalConfiguration.provides(configuration)
+        return LocalConfiguration.provides(newConfig)
     }
 }
