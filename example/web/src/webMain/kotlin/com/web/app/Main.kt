05720a1b9f6c946b96372ae609d6277868c93182
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-02-28 17:13:44 +0530

    Remove `onWasmReady` from wasm target

--- example/web/src/webMain/kotlin/com/web/app/Main.kt
+++ example/web/src/webMain/kotlin/com/web/app/Main.kt
@@ -5,19 +5,16 @@ import androidx.compose.ui.window.ComposeViewport
 import com.compose.app.WebApp
 import com.compose.app.di.initKoin
 import kotlinx.browser.document
-import org.jetbrains.skiko.wasm.onWasmReady
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     initKoin()
-    onWasmReady {
-        val app = document.getElementById(elementId = "app") ?: return@onWasmReady
 
-        ComposeViewport(viewportContainer = app) {
-            val loaderContainer = document.getElementById("loader-container")
-            loaderContainer?.remove()
+    val app = document.getElementById(elementId = "app") ?: return@onWasmReady
+    ComposeViewport(viewportContainer = app) {
+        val loaderContainer = document.getElementById("loader-container")
+        loaderContainer?.remove()
 
-            WebApp()
-        }
+        WebApp()
     }
 }
