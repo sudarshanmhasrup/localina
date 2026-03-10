d94836a4018efa600f189a86e30bab8f561a0d1a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-03-10 10:32:05 +0530

    Add "com.android.application" plugin to the root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.compose.multiplatform) apply false
     alias(libs.plugins.compose.compiler) apply false
