ba7db02fb7695ec325b88dafcbed6ff11a17e1ce
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-03-10 10:14:58 +0530

    Simplify LanguagePage layout and remove adaptive logic in ComposeApp.kt

--- example/compose/src/commonMain/kotlin/com/compose/app/presentation/composeApp/ComposeApp.kt
+++ example/compose/src/commonMain/kotlin/com/compose/app/presentation/composeApp/ComposeApp.kt
@@ -1,25 +1,16 @@
 package com.compose.app.presentation.composeApp
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.window.core.layout.WindowHeightSizeClass
-import androidx.window.core.layout.WindowSizeClass.Companion.HEIGHT_DP_MEDIUM_LOWER_BOUND
-import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
-import androidx.window.core.layout.WindowWidthSizeClass
 import com.compose.app.presentation.components.HeadingAndMessage
 import com.compose.app.presentation.components.LanguageCard
 import com.compose.app.presentation.extensions.backgroundModifier
@@ -37,7 +28,7 @@ fun ComposeApp() {
         LocalinaApp {
             val backgroundColor = MaterialTheme.colorScheme.background
             Column(modifier = Modifier.backgroundModifier(backgroundColor = backgroundColor)) {
-                LanguagePage()
+                LanguagePage(modifier = Modifier.fillMaxSize())
             }
         }
     }
@@ -46,70 +37,13 @@ fun ComposeApp() {
 @Composable
 private fun LanguagePage(modifier: Modifier = Modifier) {
     val composeAppViewModel = koinViewModel<ComposeAppViewModel>()
-
-    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
-
-    val isMobileLandscapeMode  = windowSizeClass.isWidthAtLeastBreakpoint(WIDTH_DP_MEDIUM_LOWER_BOUND) &&
-            !windowSizeClass.isHeightAtLeastBreakpoint(HEIGHT_DP_MEDIUM_LOWER_BOUND)
-
-    if (isMobileLandscapeMode) {
-        Row(modifier = modifier) {
-            val modifier1 = Modifier
-                .fillMaxHeight()
-                .weight(0.8f)
-            val modifier2 = Modifier
-                .fillMaxHeight()
-                .weight(1.2f)
-
-            LandingPageLayout(
-                isMobileLandscapeMode = true,
-                composeAppViewModel = composeAppViewModel,
-                modifier = modifier1,
-                modifier2 = modifier2,
-            )
-        }
-    } else {
-        Column(modifier = modifier) {
-            LandingPageLayout(
-                isMobileLandscapeMode = false,
-                composeAppViewModel = composeAppViewModel,
-                modifier = Modifier.fillMaxWidth(),
-                modifier2 = Modifier.fillMaxWidth()
-            )
-        }
-    }
-}
-
-@Composable
-private fun LandingPageLayout(
-    isMobileLandscapeMode: Boolean,
-    composeAppViewModel: ComposeAppViewModel,
-    modifier: Modifier = Modifier,
-    modifier2: Modifier = Modifier
-) {
-    Box(modifier = modifier) {
-        val backgroundModifier = if (isMobileLandscapeMode) {
-            Modifier
-                .headingAndMessageModifier(isMobileLandscapeMode = true)
-                .align(alignment = Alignment.Center)
-        } else {
-            Modifier.headingAndMessageModifier()
-        }
+    Column(modifier = modifier) {
         HeadingAndMessage(
             heading = Res.string.language_page_heading,
             message = Res.string.language_page_message,
-            modifier = backgroundModifier
+            modifier = Modifier.headingAndMessageModifier()
         )
-    }
-    Box(modifier = modifier2) {
-        val languageListModifier = if (isMobileLandscapeMode) {
-            Modifier
-                .fillMaxWidth()
-                .align(alignment = Alignment.Center)
-        } else {
-            Modifier.fillMaxSize()
-        }
-        LanguageList(composeAppViewModel = composeAppViewModel, modifier = languageListModifier)
+        LanguageList(composeAppViewModel = composeAppViewModel, modifier = Modifier.fillMaxSize())
     }
 }
 
