db54b76a9d8dc1ac080813ace76f525d50817a5d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-03-13 00:57:55 +0530

    Define the example:android module's configuration information from the version catalog

--- example/android/build.gradle.kts
+++ example/android/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
 }
 
+group = libs.versions.example.android.group.get()
+version = libs.versions.example.android.version.get()
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/example/desktop"))
