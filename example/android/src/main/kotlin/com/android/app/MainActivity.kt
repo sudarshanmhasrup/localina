20a7a55a352aae4a47b456b60150d4691c9ef7cd
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-03-16 21:25:42 +0530

    Update MainActivity to use AndroidApp

--- example/android/src/main/kotlin/com/android/app/MainActivity.kt
+++ example/android/src/main/kotlin/com/android/app/MainActivity.kt
@@ -3,12 +3,13 @@ package com.android.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.compose.app.AndroidApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            AndroidApp()
         }
     }
 }
