b236c2f6a45d09f8fcb07c9ceb63a5fe7bdd7889
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-03-16 09:31:01 +0530

    Apply the "org.jetbrains.kotlin.plugin.compose" plugin to the example:android module

--- example/android/build.gradle.kts
+++ example/android/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.compose.compiler)
 }
 
 group = libs.versions.example.android.group.get()
