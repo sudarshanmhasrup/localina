9a16be6a32390f45f97fda64c7a61984aaf0c903
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-03-16 21:18:08 +0530

    Downgrade JVM target to 11 in desktop example

--- example/desktop/build.gradle.kts
+++ example/desktop/build.gradle.kts
@@ -12,16 +12,8 @@ version = libs.versions.example.desktop.version.get()
 
 kotlin {
     jvm("desktop") {
-        java {
-            toolchain {
-                languageVersion.set(JavaLanguageVersion.of("21"))
-            }
-            sourceCompatibility = JavaVersion.VERSION_21
-            targetCompatibility = JavaVersion.VERSION_21
-        }
-
         compilerOptions {
-            jvmTarget = JvmTarget.JVM_21
+            jvmTarget = JvmTarget.JVM_11
         }
     }
 
