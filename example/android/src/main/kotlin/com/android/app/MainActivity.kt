abda6383ca37021daa0eed48aa3ff1d08a478b1c
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-03-16 21:30:05 +0530

    Enable edge-to-edge in MainActivity

--- example/android/src/main/kotlin/com/android/app/MainActivity.kt
+++ example/android/src/main/kotlin/com/android/app/MainActivity.kt
@@ -2,12 +2,19 @@ package com.android.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import android.graphics.Color
 import com.compose.app.AndroidApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT),
+            navigationBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT)
+        )
         setContent {
             AndroidApp()
         }
