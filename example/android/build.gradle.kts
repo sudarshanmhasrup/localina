2496b941f13cbb40bb17838f11187311436f2b2a
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-03-16 09:22:18 +0530

    Define the example:android module's android app package information from the version catalog

--- example/android/build.gradle.kts
+++ example/android/build.gradle.kts
@@ -5,5 +5,18 @@ plugins {
 group = libs.versions.example.android.group.get()
 version = libs.versions.example.android.version.get()
 
+android {
+    namespace = libs.versions.example.android.namespace.get()
+    compileSdk = libs.versions.example.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = libs.versions.example.android.applicationId.get()
+        minSdk = libs.versions.example.android.minSdk.get().toInt()
+        targetSdk = libs.versions.example.android.targetSdk.get().toInt()
+        versionCode = libs.versions.example.android.versionCode.get().toInt()
+        versionName = libs.versions.example.android.versionName.get()
+    }
+}
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/example/desktop"))
