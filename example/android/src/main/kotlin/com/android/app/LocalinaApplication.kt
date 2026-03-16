a89bcada7b76f6a9a6a252f3efc34c64d768e685
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-03-16 21:25:32 +0530

    Initialize Koin in LocalinaApplication

--- example/android/src/main/kotlin/com/android/app/LocalinaApplication.kt
+++ example/android/src/main/kotlin/com/android/app/LocalinaApplication.kt
@@ -1,9 +1,14 @@
 package com.android.app
 
 import android.app.Application
+import com.compose.app.di.initKoin
+import org.koin.android.ext.koin.androidContext
 
 class LocalinaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        initKoin {
+            androidContext(this@LocalinaApplication)
+        }
     }
 }
