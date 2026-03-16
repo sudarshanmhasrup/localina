1f19e5ddd4d933257304077fdd0e93ea3a3698e2
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2026-03-16 21:24:51 +0530

    Add status and navigation bar padding to screenModifier

--- example/compose/src/commonMain/kotlin/com/compose/app/presentation/extensions/Modifiers.kt
+++ example/compose/src/commonMain/kotlin/com/compose/app/presentation/extensions/Modifiers.kt
@@ -3,7 +3,9 @@ package com.compose.app.presentation.extensions
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -14,6 +16,8 @@ internal fun Modifier.backgroundModifier(backgroundColor: Color): Modifier {
     return this
         .fillMaxSize()
         .background(color = backgroundColor)
+        .statusBarsPadding()
+        .navigationBarsPadding()
 }
 
 internal fun Modifier.headingAndMessageModifier(): Modifier {
